dc85706e2cab953a1ac996f8e3d793daf51f6b03
Heinsenbergg <chromaticechoo@gmail.com>
2024-01-08 11:46:47 -0500

    first commit

---
+++ hardware_software_code.py
@@ -0,0 +1,12 @@
+def main():
+    name="Eric"
+    print("What is your name?", end =" ")
+    answer = input()
+    print("We want to know if you like programming!")
+    print("Do you like programming "+ name + "?", end =" ")
+    answer = input()
+    print("Great! You said " + answer + "?")
+    print("Let's start learning some Python today")
+
+if __name__ == "__main__":
+    main()
