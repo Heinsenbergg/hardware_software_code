b5a4100390a181bfea0025fbabe7d662d4d6373a
Heinsenbergg <chromaticechoo@gmail.com>
2024-01-10 11:17:17 -0500

    conversation

--- Whatisyourname.py
+++ Whatisyourname.py
@@ -1,8 +1,8 @@
 def main():
     name = input("What is your name?")
     print("We want to know if you like programming!")
-    answer = input("Do you like programming"+ name + "?")
-    print('Great! You said "' + answer + '"?')
+    answer = input("Do you like programming "+ name + "?")
+    print('Great! You said, "' + answer + '!"?')
     print("Let's start learning some Python today")
 
 if __name__ == "__main__":
