10ab304750080fa37caa1d75d0de22a6bc9a8ce1
Heinsenbergg <chromaticechoo@gmail.com>
2024-01-22 11:38:29 -0500

    2024122 HtS CLass

--- project1.py
+++ project1.py
@@ -1,5 +1,19 @@
 def main():
     print("This can convert a decimal number to binary and a binary number to decimal. ")
+    print("Choose The Following Conversation")
+    print("(1) Binary to Decimal")
+    print("(2) Decimal To Binary")
+    print("(3) Binary to Hexadecimal")
+    print("(4) Decimal to Hexadecimal")
+    print("(5) Hexadecimal to Decimal")
+    print("(6) Hexadecimal to Binary")
+    print("(7) Octal to Decimal")
+    print("(8) Decimal to Octal")
+    return input("Enter a Valid Selection:")
+    if answer ==
+run =
+    print("Enter a Valid Selection")
+    elif answer == " "
 
 
 if __name__ == "__main__":
