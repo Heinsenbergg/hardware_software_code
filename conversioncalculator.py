d025820fc02174f9a779de602b04c19b7572a356
Heinsenbergg <chromaticechoo@gmail.com>
2024-02-15 20:51:04 -0500

    20240215 hts class

--- conversioncalculator.py
+++ conversioncalculator.py
@@ -1,5 +1,7 @@
 #Eric Lopez
+#This is a conversion calculator and it converts Decimal to Binary and Binary to Decimal
 def execute_display(menu_dict, title):
+    #shows title and options
     print(title)
     for items, values in menu_dict.items():
         print("{}. {}".format(items, values.capitalize()))
@@ -14,13 +16,14 @@ def execute_display(menu_dict, title):
     return selection, menu_dict[selection]
 
 def menu_display():
+    #menu options for user
     menu_dict = {
         '1': 'decimal to binary',
         '2': 'binary to decimal',
         'x': 'exit program'
     }
     return menu_dict
-
+#converts decimal to binary
 def decimal_to_binary(number):
     result = ""
     number = int(number)
@@ -31,10 +34,12 @@ def decimal_to_binary(number):
     return result
 
 def decimal():
+    #takes decimal and tuns into binary
     num = input("Enter Decimal Number:")
     print("Decimal {} to Binary: {}".format(num, decimal_to_binary(num)))
 
 def binary_to_decimal(number):
+    #converts binary number to decimal
     result = 0
     if len(number) > 0:
         power = len(str(number)) - 1
@@ -43,18 +48,19 @@ def binary_to_decimal(number):
             power -= 1
         return result
 
-def bin():
+def bin():#takes binary input and turns into decimal
     num = input("Enter Binary Number:")
     print("Binary to Decimal: {}".format(binary_to_decimal(num)))
 
 def check_selection():
-    # Function placeholder for future implementation
+
     pass
 
 def main():
+    #start of code
     title = "Conversion Calculator"
 
-    while True:
+    while True:#shows the menu and gets input
         sel, choice = execute_display(menu_display(), title)
         if sel == '1':
             decimal()
@@ -65,7 +71,7 @@ def main():
             break
         else:
             print("Invalid selection.")
-        input("Press Enter to continue")
+        input("Press Enter to continue")# allows person to redo code again 
 
 if __name__ == "__main__":
     main()
